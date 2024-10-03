760f135f1f57e247f1b4fa390dc29391562043fb
somil panwar <122174329+somilpanwar@users.noreply.github.com>
2024-10-03 22:23:10 +0530

    Add: adding icon in UI

--- constant/index.ts
+++ constant/index.ts
@@ -1,21 +1,26 @@
+
 export const links =[
     {
         route:"/",
+        icon:"/icon/house-solid.svg",
         label:"Home"
 
     },
     {
         route:"/Upcoming",
+        icon:"",
         label:"Upcoming"
 
     },
     {
         route:"/personalroom",
+       icon:"",
         label:"PersonalRoom"
 
     },
     {
         route:"/previous",
+        icon:"",
         label:"Previous"
 
     },
--- src/components/sidebarlinks.tsx
+++ src/components/sidebarlinks.tsx
@@ -4,6 +4,9 @@ import { links } from '../../constant'
 import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
+import Image from 'next/image'
+
+
 
 const Sidebarlinks = () => {
     const pathname = usePathname();
@@ -13,13 +16,24 @@ const Sidebarlinks = () => {
      {links.map((e,id)=>{
        const isActive=pathname==e.route;
         return(
+          <div
+          key={id}>
+
+   
+        
             <Link 
-            key={id}
             href={e.route}
             className={cn('h-10 p-2 pl-9 pr-9  text-white font-bold text-center rounded-lg ',{'bg-green-400':isActive})}
             > 
+            <Image 
+            src={e.icon}
+            width={32}
+            height={32}
+            alt='home'
+            ></Image>
             {e.label}
             </Link>
+            </div>
         )
      })}
 
